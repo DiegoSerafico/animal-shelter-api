5ae72f991625a1c06ac7c85bbdb5be240ed34cf9
Diego Serafico <dseraficohernandez@gmail.com>
2021-10-29 11:22:15 -0700

    update animal model to require fields with range

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -1,11 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System;
+
 namespace AnimalShelter.Models
 {
     public class Animal
     {
         public int AnimalId { get; set; }
+
+        [Required]
+        [StringLength(20)]
         public string Name { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Species { get; set; }
-        public int Age { get; set; }
+
+        [Required]
+        [StringLength(10)]
         public string Gender { get; set; }
+
+        [Required]
+        [Range(0, Double.PositiveInfinity)]
+        public int Age { get; set; }
     }
 }
